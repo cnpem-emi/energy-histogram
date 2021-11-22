e7032516cbcdd76ed42530c1030cf6b50fc73d8b
root <root@BBB-NEW-2021.abtlus.org.br>
2021-11-22 09:33:34 -0200

    Added list of total counts

--- BeagleBone/main.py
+++ BeagleBone/main.py
@@ -11,8 +11,8 @@ list_adc = [0]*4096
 
 ADC.setup()
 
-GPIO.setup(capDischarge, GPIO.OUT)
-GPIO.output(capDischarge, GPIO.LOW)
+#GPIO.setup(capDischarge, GPIO.OUT)
+#GPIO.output(capDischarge, GPIO.LOW)
 
 GPIO.setup(clearFlipFlop, GPIO.OUT)
 GPIO.output(clearFlipFlop, GPIO.HIGH)
@@ -20,25 +20,31 @@ GPIO.output(clearFlipFlop, GPIO.HIGH)
 GPIO.setup(triggerInput, GPIO.IN)
 
 client = StrictRedis(host = "127.0.0.1")
-client.lpush("gammaenergy", 0, 0)
+client.lpush("gamma_energy", 0, 0)
+
+for i in range(4096):
+    client.lpush("gamma_energy_total")
 
 while(True):
     while(not(GPIO.input(triggerInput))): # Waiting to receive start pulse
         pass
 
-    capDischarge_sleep = float(client.get("capDischarge_sleep"))
+    capDischarge_sleep = float(client.get("capacitor_sleep"))
 
     adc_value = int(ADC.read(voltageInput)*4096)
     list_adc[adc_value] += 1
 
-    GPIO.output(capDischarge, GPIO.HIGH)
+#    GPIO.output(capDischarge, GPIO.HIGH)
     GPIO.output(clearFlipFlop, GPIO.LOW)
 
     sleep(capDischarge_sleep)
 
-    client.lset("gammaenergy", 0, adc_value)
-    client.lset("gammaenergy", 1, list_adc[adc_value])
+    client.lset("gamma_energy", 0, adc_value)
+    client.lset("gamma_energy", 1, list_adc[adc_value])
 
-    GPIO.output(capDischarge, GPIO.LOW)
+
+    client.lset("gamma_energy_total", adc_value, list_adc[adc_value])
+
+#    GPIO.output(capDischarge, GPIO.LOW)
     GPIO.output(clearFlipFlop, GPIO.HIGH)
 
